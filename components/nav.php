2b0734f6d62c527fbeb155024559b146a242a42f
NicXVII <diminich.nicolas@volta.ts.it>
2024-04-27 09:53:00 +0200

    first commit

---
+++ components/nav.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" href="resources/css/nav.css">
+
+<nav>
+    <ul>
+        <li>
+            <a href="index.php">
+                <img src="path/to/your/logo.jpg" alt="Your Website Logo">
+            </a>
+        </li>
+        <li>
+            <a href="#">Skills</a>
+        </li>
+        <li>
+            <a href="#">Projects</a>
+        </li>
+        <li>
+            <a href="#">Contact</a>
+        </li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Diminich Nicoals</title>
+    <link rel="stylesheet" href="resources/css/index.css">
+</head>
+
+<body>
+    <?php
+    require_once('components/nav.php');
+    ?>
+    <div class="phrase">
+        <h1>Young, creative and innovative</h1>
+    </div>
+    <div class="presentation">
+
+    </div>
+    <div class="lastPhrase">
+
+    </div>
+</body>
+
+</html>
