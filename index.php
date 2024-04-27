d919c3c475548dffbe10491e051de1bba7a1c044
NicXVII <diminich.nicolas@volta.ts.it>
2024-04-27 10:08:51 +0200

    some responsive stuff
    create responsive for nav and invex, still need improvement

--- index.php
+++ index.php
@@ -16,10 +16,10 @@
         <h1>Young, creative and innovative</h1>
     </div>
     <div class="presentation">
-
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra lectus ut congue consectetur. Nullam id mauris et libero dapibus placerat. Ut mollis convallis felis, eu rhoncus massa elementum eu. Morbi nec tortor in elit pulvinar ultrices. Quisque lobortis varius sem non euismod. Maecenas posuere, nisi sed rhoncus dapibus, libero odio luctus arcu, vitae ultricies dui nunc sit amet justo. Donec mollis sapien nec mauris gravida, vitae venenatis metus accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris vitae imperdiet massa. Curabitur nec dignissim ligula. Etiam non bibendum arcu. Aenean scelerisque, turpis et aliquet semper, mauris justo ullamcorper elit, non bibendum felis sapien eget est.</p>
     </div>
     <div class="lastPhrase">
-
+        <p>this is Nicolas Diminich</p>
     </div>
 </body>
 
